9c81c7c0713f3fa6b10c87deeb6705356cbfa0e8
Moises Yacila Palacios <myacilapalacios@horizon.csueastbay.edu>
2021-08-18 11:44:07 -0700

    Added basic express app, and set up EJS

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+//Require dependencies and call express
 const express = require('express');
+const path = require('path');
 const app = express();
 
+//Set up our views folder for templates, and public directory to serve static files.
+//Thanks to our path.join we can run the file from anywhere in the computer
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+//We need to include this middleware to get req.body from POST requests
+app.use(express.urlencoded({ extended: true }));
+
+//SSet up template engine as EJS
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
