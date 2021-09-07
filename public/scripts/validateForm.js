601fba8072d8d8c1dd340b869edf1d24f829f31b
Moises Yacila Palacios <myacilapalacios@horizon.csueastbay.edu>
2021-09-07 01:09:15 -0700

    Added Bootstrap code for client side form validation

---
+++ public/scripts/validateForm.js
@@ -0,0 +1,24 @@
+// IIFE (Immediately Invoked Function Expression) syntax:
+// Wrap an annonymous function in parenthesis, and add another set of parenthesis to call it
+// For more go to MDN and check IIFE
+
+// CODE FROM BOOTSTRAP: Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.validated-form')
+
+    // Loop over them and prevent submission
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
